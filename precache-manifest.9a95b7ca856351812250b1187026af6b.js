105377a29509d4f141ee51d6cdc4e75ede00653c
shahir-abdullah <shahir2561@gmail.com>
2020-07-19 21:53:08 +0600

    Updates

---
+++ precache-manifest.9a95b7ca856351812250b1187026af6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f02690f15eb038093c907d885cb7aec",
+    "url": "/cheez/index.html"
+  },
+  {
+    "revision": "3d643809276151e16c60",
+    "url": "/cheez/static/css/main.97a87ce8.chunk.css"
+  },
+  {
+    "revision": "28fd0506cd59263726cb",
+    "url": "/cheez/static/js/2.368b28c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cheez/static/js/2.368b28c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d643809276151e16c60",
+    "url": "/cheez/static/js/main.250387c5.chunk.js"
+  },
+  {
+    "revision": "58e40db5a02cc3dfa37b",
+    "url": "/cheez/static/js/runtime-main.97518f7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cheez/static/media/logo.5d5d9eef.svg"
+  }
+]);
